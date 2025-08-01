494768110991adc00acca7e0b24c096dc73d879e
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-02 00:59:14 +0600

    reverse added

---
+++ reverse.js
@@ -0,0 +1,12 @@
+const sentence = 'I am learning web dev.' 
+// console.log(sentence);
+
+
+let reverse= ""
+for (const letter of sentence) {
+    // console.log(letter);
+    reverse = letter + reverse;
+       
+}
+console.log(reverse);
+
