bf122c1ef9bd09f36d120444a7a68b8494430934
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-01 23:07:01 +0600

    case and trim added

---
+++ index.js
@@ -0,0 +1,30 @@
+// case-------
+const school = 'RAJ uttara model school';
+//  console.log(school);
+//  console.log(school.toUpperCase());
+
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log('We are same');
+    
+}
+else{
+    console.log('We are not same');
+    
+}
+
+// ----------trim------------
+const drink = 'water';
+const liquid = ' water ';
+
+if(drink.trim() === liquid.trim()){
+    console.log('pani er opor naam life');
+
+}
+else{
+    console.log('sagore pani ache but khawa jai na');
+    
+}
