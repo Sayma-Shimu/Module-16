e773e686a316b06404843951c8fce6d0a05ea02d
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-02 13:08:06 +0600

    nested object

---
+++ keys.js
@@ -0,0 +1,13 @@
+const computer = {
+    brand: 'walton',
+    price: 65000,
+    processor: 'intel',
+    hdd: '512gb'
+} 
+
+const koitaKeyAche = Object.keys(computer);
+// console.log(koitaKeyAche);
+
+// value dekte same---------------
+const koitaValueAche = Object.values(computer);
+console.log(koitaValueAche);
